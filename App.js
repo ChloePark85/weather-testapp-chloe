1a6774ac950a589da221be6900b8c92380c2aa65
ChloePark85 <hapark85@gmail.com>
2020-12-29 00:29:05 +0900

    added geolocation code

--- App.js
+++ App.js
@@ -1,9 +1,18 @@
 
 import React from 'react';
 import Loading from "./Loading";
+import * as Location from "expo-location";
+import { render } from 'react-dom';
 
-export default function App() {
-  return <Loading />
+export default class extends React.Component{
+  getLocation = async () => {
+    const location = await Location.getCurrentPositionAsync()
+    console.log(location);
+  };
+  componentDidMount(){
+    this.getLocation();
+  }
+render(){
+  return <Loading />}
 }
-
 
