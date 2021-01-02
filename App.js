8f3844bb7c46403a54df22ce4534f2ad0303df80
ChloePark85 <hapark85@gmail.com>
2021-01-02 23:33:53 +0900

    getting location

--- App.js
+++ App.js
@@ -1,18 +1,32 @@
-
 import React from 'react';
+import {Alert }from "react-native";
 import Loading from "./Loading";
 import * as Location from "expo-location";
 import { render } from 'react-dom';
 
 export default class extends React.Component{
+  state = {
+    isLoading: true
+  }
+
   getLocation = async () => {
-    const location = await Location.getCurrentPositionAsync()
-    console.log(location);
+    try {
+      await Location.requestPermissionsAsync();
+      const {coords: { latitude, longitude}} = await Location.getCurrentPositionAsync();
+      // send to API
+      this.setState({ isLoading: False})
+
+  } catch(error){
+      Alert.alert("찾을 수 없네요ㅜㅜ");
+  }
+  
+    
   };
   componentDidMount(){
     this.getLocation();
   }
 render(){
-  return <Loading />}
+  const { isLoading } = this.state;
+  return isLoading? <Loading /> : null;
 }
-
+}
